43df8a0ee11e5731739c75b24201d783be2737ce
David Teller <D.O.Teller@gmail.com>
2016-12-16 10:57:39 +0100

    Initial Commit

---
+++ Lighthouse Camera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lighthouse Camera
+//
+//  Created by David Teller on 16/12/16.
+//  Copyright © 2016 Lighthouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
