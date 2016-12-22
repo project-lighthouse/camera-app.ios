8ade5df1f249e4ff50c542e00af5296c1461f5ad
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2016-12-22 13:23:16 +0100

    Simplify UIImage<->cv::Mat conversion methods.

--- Lighthouse Camera/Bridge/bridge.mm
+++ Lighthouse Camera/Bridge/bridge.mm
@@ -15,72 +15,60 @@ using namespace cv;
     return [self matrixToImage:outputMatrix andImageOrientation:[input imageOrientation]];
 }
 
+// Converts UIImage instance into cv::Mat object that is known for OpenCV.
 -(cv::Mat)imageToMatrix:(UIImage *) image {
-    CGImageRef imageRef = image.CGImage;
-
-    const int srcWidth = (int) CGImageGetWidth(imageRef);
-    const int srcHeight = (int) CGImageGetHeight(imageRef);
-
-    CGDataProviderRef provider = CGImageGetDataProvider(imageRef);
-    CFDataRef data = CGDataProviderCopyData(provider);
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
 
-    cv::Mat rgbaContainer(srcHeight, srcWidth, CV_8UC4);
-    CGContextRef context = CGBitmapContextCreate(rgbaContainer.data,
-            srcWidth,
-            srcHeight,
-            8,
-            4 * srcWidth,
-            colorSpace,
-            kCGImageAlphaNoneSkipLast | kCGBitmapByteOrder32Big);
+    CGFloat cols = image.size.width;
+    CGFloat rows = image.size.height;
 
-    CGContextDrawImage(context, CGRectMake(0, 0, srcWidth, srcHeight), imageRef);
+    cv::Mat matrix(rows, cols, CV_8UC4);
+    CGContextRef context = CGBitmapContextCreate(matrix.data,
+            cols,
+            rows,
+            8,
+            matrix.step[0],
+            colorSpace,
+            kCGImageAlphaNoneSkipLast | kCGBitmapByteOrderDefault);
+
+    CGContextDrawImage(context, CGRectMake(0, 0, cols, rows), image.CGImage);
 
     CGContextRelease(context);
     CGColorSpaceRelease(colorSpace);
-    CGDataProviderRelease(provider);
-    CFRelease(data);
-
-    cv::Mat matrix;
-    cv::cvtColor(rgbaContainer, matrix, cv::COLOR_RGBA2BGRA);
 
     return matrix;
 }
 
 
+// Converts cv::Mat object into UIImage instance preserving original orientation.
 - (UIImage *)matrixToImage:(const cv::Mat &)matrix andImageOrientation:(UIImageOrientation)orientation {
-    cv::Mat rgbaView;
+    NSData *data = [NSData dataWithBytes:matrix.data length:matrix.elemSize() * matrix.total()];
 
-    if (matrix.channels() == 3) {
-        cv::cvtColor(matrix, rgbaView, cv::COLOR_BGR2RGBA);
-    } else if (matrix.channels() == 4) {
-        cv::cvtColor(matrix, rgbaView, cv::COLOR_BGRA2RGBA);
-    } else if (matrix.channels() == 1) {
-        cv::cvtColor(matrix, rgbaView, cv::COLOR_GRAY2RGBA);
+    CGColorSpaceRef colorSpace;
+    if (matrix.elemSize() == 1) {
+        colorSpace = CGColorSpaceCreateDeviceGray();
+    } else {
+        colorSpace = CGColorSpaceCreateDeviceRGB();
     }
 
-    NSData *data = [NSData dataWithBytes:rgbaView.data length:rgbaView.elemSize() * rgbaView.total()];
-
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-
-    CGDataProviderRef provider = CGDataProviderCreateWithCFData((__bridge CFDataRef) data);
+    CGDataProviderRef provider = CGDataProviderCreateWithCFData((__bridge CFDataRef)data);
 
     // Creating CGImage from cv::Mat
     CGImageRef imageRef = CGImageCreate(
             // Image width.
-            rgbaView.cols,
+            matrix.cols,
             // Image height.
-            rgbaView.rows,
+            matrix.rows,
             // Bits per component.
             8,
             // Bits per pixel.
-            8 * rgbaView.elemSize(),
+            8 * matrix.elemSize(),
             // Bytes per row.
-            rgbaView.step.p[0],
+            matrix.step[0],
             // Color space.
             colorSpace,
             // Bitmap info.
-            kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big,
+            kCGImageAlphaNone | kCGBitmapByteOrderDefault,
             // CGDataProvider reference.
             provider,
             // Decode.
@@ -92,12 +80,12 @@ using namespace cv;
     );
 
     // Getting UIImage from CGImage
-    UIImage *finalImage = [UIImage imageWithCGImage:imageRef scale:1 orientation:orientation];
+    UIImage *image = [UIImage imageWithCGImage:imageRef scale:1 orientation:orientation];
 
     CGImageRelease(imageRef);
     CGDataProviderRelease(provider);
     CGColorSpaceRelease(colorSpace);
 
-    return finalImage;
+    return image;
 }
 @end
