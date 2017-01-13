09ec9866aec69ed60a905d699732952b5ddad822
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2017-01-13 15:48:33 +0100

    Changed default Swift code indent to 2 spaces.

--- Lighthouse Camera/UI/Main/ViewController.swift
+++ Lighthouse Camera/UI/Main/ViewController.swift
@@ -10,79 +10,79 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    var bridge: Bridge!
+  var bridge: Bridge!
 
-    var isBusy: Bool
+  var isBusy: Bool
 
-    required init?(coder aCoder: NSCoder) {
-        isBusy = false
-        super.init(coder:aCoder)
+  required init?(coder aCoder: NSCoder) {
+    isBusy = false
+    super.init(coder: aCoder)
+  }
+
+  //MARK: Properties
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var label: UILabel!
+
+  //MARK: Actions
+
+  // Invoked when the user has clicked on "record".
+  @IBAction func onRecordClick(_ sender: Any) {
+    if self.isBusy {
+      bridge.onStopCapture()
+      self.isBusy = false
+
+      return;
     }
 
-    //MARK: Properties
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var label: UILabel!
-
-    //MARK: Actions
-
-    // Invoked when the user has clicked on "record".
-    @IBAction func onRecordClick(_ sender: Any) {
-        if self.isBusy {
-            bridge.onStopCapture()
-            self.isBusy = false
-
-            return;
-        }
-
-      // Ask for microphone permission beforehand so that user is not interrupted when he tries to record voice label
-      // for the first time.
-      AVAudioSession.sharedInstance().requestRecordPermission({(granted: Bool)-> Void in
-          if granted {
-              self.bridge.onRecordObject();
-              self.isBusy = true
-          } else{
-              self.showError(message: "If you want to add or edit voice labels for the images, please, go to " +
-                  "Settings > Privacy > Microphone and enable microphone permission for the Lighthouse app.")
-          }
-      })
-    }
-
-    // Invoked when the user clicks to stop the ongoing action.
-    @IBAction func onEmptyClick(_ sender: Any) {
-        bridge.onStopCapture();
-    }
-
-    // Invoked when the user has clicked on "identify".
-    @IBAction func onIdentifyClick(_ sender: Any) {
-      if self.isBusy {
-        bridge.onStopCapture()
-        self.isBusy = false
-      } else {
-        bridge.onIdentifyObject();
+    // Ask for microphone permission beforehand so that user is not interrupted when he tries to record voice label
+    // for the first time.
+    AVAudioSession.sharedInstance().requestRecordPermission({ (granted: Bool) -> Void in
+      if granted {
+        self.bridge.onRecordObject();
         self.isBusy = true
+      } else {
+        self.showError(message: "If you want to add or edit voice labels for the images, please, go to " +
+            "Settings > Privacy > Microphone and enable microphone permission for the Lighthouse app.")
       }
-    }
+    })
+  }
 
-    private func showError(message: String) {
-        let alert = UIAlertController(title: "Error", message: message,
-            preferredStyle: UIAlertControllerStyle.alert)
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default))
+  // Invoked when the user clicks to stop the ongoing action.
+  @IBAction func onEmptyClick(_ sender: Any) {
+    bridge.onStopCapture();
+  }
 
-        self.present(alert, animated: true, completion: nil)
+  // Invoked when the user has clicked on "identify".
+  @IBAction func onIdentifyClick(_ sender: Any) {
+    if self.isBusy {
+      bridge.onStopCapture()
+      self.isBusy = false
+    } else {
+      bridge.onIdentifyObject();
+      self.isBusy = true
     }
-    
-    @objc(operationComplete)
-    public dynamic func operationComplete() {
-        self.isBusy = false
-    }
+  }
 
-    // Used by `Feedback` to show frames currently being recorded
-    // from the camera.
-    @objc(showFrame:)
-    public dynamic func showFrame(frame: UIImage) {
-      NSLog("ViewController.showFrame %f x %f\n", frame.size.width * frame.scale, frame.size.height * frame.scale);
-      self.imageView.image = frame;
-    }
+  private func showError(message: String) {
+    let alert = UIAlertController(title: "Error", message: message,
+        preferredStyle: UIAlertControllerStyle.alert)
+    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default))
+
+    self.present(alert, animated: true, completion: nil)
+  }
+
+  @objc(operationComplete)
+  public dynamic func operationComplete() {
+    self.isBusy = false
+  }
+
+  // Used by `Feedback` to show frames currently being recorded
+  // from the camera.
+  @objc(showFrame:)
+  public dynamic func showFrame(frame: UIImage) {
+    NSLog("ViewController.showFrame %f x %f\n", frame.size.width * frame.scale, frame.size.height * frame.scale);
+    self.imageView.image = frame;
+  }
 
   @objc(showText:)
   public dynamic func showText(text: NSString) {
@@ -90,31 +90,31 @@ class ViewController: UIViewController {
     self.label.text = text as String;
   }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        bridge = Bridge()
-        self.isBusy = false;
-        sViewController = self;
+    bridge = Bridge()
+    self.isBusy = false;
+    sViewController = self;
 
-        self.registerSettingsBundle()
+    self.registerSettingsBundle()
 
-      // Establish audio session.
-      // FIXME: We should probably start session only when we really need it and stop when we don't.
-      let session = AVAudioSession.sharedInstance()
-      try! session.setCategory(AVAudioSessionCategoryPlayAndRecord,
-          with: AVAudioSessionCategoryOptions.defaultToSpeaker)
-      try! session.setActive(true)
-    }
+    // Establish audio session.
+    // FIXME: We should probably start session only when we really need it and stop when we don't.
+    let session = AVAudioSession.sharedInstance()
+    try! session.setCategory(AVAudioSessionCategoryPlayAndRecord,
+        with: AVAudioSessionCategoryOptions.defaultToSpeaker)
+    try! session.setActive(true)
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
 
-    func registerSettingsBundle() {
-        let defaults = UserDefaults.standard
+  func registerSettingsBundle() {
+    let defaults = UserDefaults.standard
 
-        defaults.set(500, forKey: "Matching:NumberOfFeatures")
-    }
+    defaults.set(500, forKey: "Matching:NumberOfFeatures")
+  }
 }
 
