a95cf2229ae34c544a7d58e1f797aa4000556d73
David Teller <D.O.Teller@gmail.com>
2017-01-24 23:19:58 +0100

    [WIP] Working on tests

--- Lighthouse CameraTests/Lighthouse_CameraTests.swift
+++ Lighthouse CameraTests/Lighthouse_CameraTests.swift
@@ -24,6 +24,7 @@ class Lighthouse_CameraTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+      XCTAssert(false, "THIS SHOULD FAIL");
     }
     
     func testPerformanceExample() {
