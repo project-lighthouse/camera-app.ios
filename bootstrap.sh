4cd5e98472dffa13706d8152d788588b79597c77
David Teller <D.O.Teller@gmail.com>
2017-01-28 22:09:27 +0100

    Bootstrap: Attempting to install brew if necessary.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,32 @@
 #!/usr/bin/env bash
 
-# Installing CocoaPods if necessary
+# Install dependencies that are needed by CocoaPods.
+
+# OpenCV needs cmake.
+if !which cmake > /dev/null; then
+  echo "Lighthouse: Attempting to install cmake."
+  if !which port > /dev/null; then
+    echo "Lighthouse: Installing cmake with macports."
+    sudo port install cmake
+  elif !which brew > /dev/null; then
+    echo "Lighthouse: Installing cmake with brew."
+    sudo brew install cmake
+  else
+    echo "Lighthouse: Installing brew."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    echo "Lighthouse: Installing cmake with brew."
+    sudo brew install cmake
+  fi
+fi
+
+# Install CocoaPods.
 if ! which pod >/dev/null; then
-  echo "Lighthouse: Installing CocoaPods"
+  echo "Lighthouse: Installing CocoaPods."
   sudo gem install cocoapods
 fi
 
-# Installing dependencies through CocoaPods
+# Installing further dependencies through CocoaPods.
 echo "Lighthouse: Installing dependencies through CocoaPods."
 echo "Lighthouse: Go and fetch a cup of tea, this may take a while."
 pod install
