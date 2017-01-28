f03186c086ec4c99c62368ac82169aaa76879d3c
David Teller <D.O.Teller@gmail.com>
2017-01-28 21:36:55 +0100

    More warningsxs

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Install dependencies if necessary
-if ! which swiftlint >/dev/null; then
-    brew install swiftlint
-fi
+# if ! which swiftlint >/dev/null; then
+#    brew install swiftlint
+# fi
 
 if ! which cmake >/dev/null; then
     brew install cmake
